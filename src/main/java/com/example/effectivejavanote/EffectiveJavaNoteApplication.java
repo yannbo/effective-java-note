fbed24445a951dc310bf6a85ca1cca6e70fd8651
apple <200802690@qq.com>
2019-10-27 22:54:49 +0800

    first commit for effective java note

---
+++ src/main/java/com/example/effectivejavanote/EffectiveJavaNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.effectivejavanote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJavaNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/effectivejavanote/EffectiveJavaNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.effectivejavanote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
