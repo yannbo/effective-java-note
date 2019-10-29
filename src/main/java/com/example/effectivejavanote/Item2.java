826df7c7108cf9463ea929978b61a72e88ff3d36
apple <200802690@qq.com>
2019-10-29 23:54:20 +0800

    Topic 2

---
+++ src/main/java/com/example/effectivejavanote/Item2.java
@@ -0,0 +1,26 @@
+package com.example.effectivejavanote;
+
+/**
+ * @ClassName Item2
+ * @Description: 第二条： 遇到多个构造器时要考虑使用构建器
+ * @Author yann
+ * @Date 2019/10/29
+ * @Version V1.0
+ **/
+public class Item2 {
+//TODO
+}
+
+/**
+ * Summary:
+ *  1. the builder copy the object filed.
+ *  2. the builder field set method return this.
+ *  3. the object constructor accept builder object.
+ *  4. the build method return the above object by the above constructor.
+ *  5. set the object field value according to the corresponding builder object field.
+ *
+ * 优点：易于阅读和编写
+ *
+ */
+
+
